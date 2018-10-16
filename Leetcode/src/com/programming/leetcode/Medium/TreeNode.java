23da0e755e9edcbe246da157f4960df44f121b2e
RahilModi <rmodi10@gmail.com>
2018-10-15 21:26:51 -0700

    more problems

---
+++ Leetcode/src/com/programming/leetcode/Medium/TreeNode.java
@@ -0,0 +1,10 @@
+package com.programming.leetcode.Easy;
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+
+}
---
+++ Leetcode/src/com/programming/leetcode/Medium/WordSearch.java
@@ -0,0 +1,4 @@
+package com.programming.leetcode.Medium;
+
+public class WordSearch {
+}
