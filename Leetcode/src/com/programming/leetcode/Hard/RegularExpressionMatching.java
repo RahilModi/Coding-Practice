6d094a3c802d20fb69931ace6a2de0dbbbfd7d5a
RahilModi <rmodi10@gmail.com>
2019-03-05 08:03:38 -0800

    practice...

--- Leetcode/src/com/programming/leetcode/Hard/RegularExpressionMatching.java
+++ Leetcode/src/com/programming/leetcode/Hard/RegularExpressionMatching.java
@@ -40,6 +40,34 @@ public class RegularExpressionMatching {
         return dp[s.length()][p.length()];
     }
 
+    public boolean isMatchV2(String s, String p) {
+        if (s == null || p == null) {
+            return false;
+        }
+        boolean[][] dp = new boolean[s.length()+1][p.length()+1];
+        dp[0][0] = true;
+        for (int i = 1; i <= p.length(); i++) {
+            if (p.charAt(i-1) == '*' && i > 1 &&  dp[0][i-2]) {
+                dp[0][i] = true;
+            }
+        }
+        for (int i = 1 ; i <= s.length(); i++) {
+            for (int j = 1; j <= p.length(); j++) {
+                if (p.charAt(j-1) == '.' || p.charAt(j-1) == s.charAt(i-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                if (p.charAt(j-1) == '*') {
+                    if (p.charAt(j-2) != s.charAt(i-1) && p.charAt(j-2) != '.') {
+                        dp[i][j] = dp[i][j-1];
+                    } else {
+                        dp[i][j] = (dp[i][j-1] || dp[i-1][j] || dp[i][j-2]);
+                    }
+                }
+            }
+        }
+        return dp[s.length()][p.length()];
+    }
+
     public boolean isMatchRecursive(String s, String p) {
         return  isMatchHelper(s, p, 0,0);
     }
